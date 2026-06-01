eef0f58c0d4c2efff6c201ae76b937f5176b1490
duyloi140205-png <duyloi140205@gmail.com>
2026-06-01 16:18:44 +0700

    feat(challenge_1): add scaffold models and database connection

---
+++ BACKEND CHALLENGES/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BACKEND_CHALLENGES.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public string? AvatarUrl { get; set; }
+
+    public int RoleId { get; set; }
+
+    public bool? IsActive { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual ICollection<Course> Courses { get; set; } = new List<Course>();
+
+    public virtual ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+
+    public virtual Role Role { get; set; } = null!;
+
+    public virtual UserAuth? UserAuth { get; set; }
+}
